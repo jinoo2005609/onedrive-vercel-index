5b054c0138821065cbf55e5532b448b82c666ad5
Esherloon <jinoo2005609@gmail.com>
2022-02-04 18:33:04 +0900

    Fix site footer

--- config/site.config.js
+++ config/site.config.js
@@ -35,7 +35,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    'Esherloon''s Server (에셜룬의 서버) | <a class="hover:border-b-2" href="https://esherloon.com/" target="_blank" rel="noopener noreferrer">esherloon.com</a>',
+    "Esherloon's Server (에셜룬의 서버) | <a class=\"hover:border-b-2\" href=\"https://esherloon.com/\" target=\"_blank\" rel=\"noopener noreferrer\">esherloon.com</a>",
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
